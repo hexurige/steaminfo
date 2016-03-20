8fe2e49c8fe4505ba1e2002f731fa201ed2da084
Xu He <xhe585@gmail.com>
2016-03-21 06:41:09 +1300

    Get app's prices

--- Steaminfo.py
+++ Steaminfo.py
@@ -6,6 +6,8 @@ https://developer.valvesoftware.com/wiki/Steam_Web_API#GetNewsForApp_.28v0001.29
 import json
 import urllib.request
 
+DOMAIN_ = "http://api.steampowered.com"
+
 class SteamAPI:
 	def __init__(self):
 		self.key = self.getApiKey()
@@ -17,9 +19,10 @@ class SteamAPI:
 		except FileNotFoundError:
 			print('key file not found')
 
+	#return apps dictionary{appid, app name}
 	def getAppsDict(self):
 		appdict = {}
-		url = 'http://api.steampowered.com/ISteamApps/GetAppList/v2'
+		url = '{domain}/ISteamApps/GetAppList/v2'.format(domain=DOMAIN_)
 		response = urllib.request.urlopen(url).read().decode('gbk', 'ignore')
 		data = json.loads(response)
 		for app in data['applist']['apps']:
@@ -28,6 +31,16 @@ class SteamAPI:
 			appdict[appids] = names
 		return appdict
 
+	#http://api.steampowered.com/ISteamEconomy/GetAssetPrices/v0001/?appid=440&key=075F586AC2F92DBA876BF91656E0FFFF
+	def getAppsPriceDict(self, appid):
+		url = '{domain}/ISteamEconomy/GetAssetPrices/v0001/?appid={id}&key={key}'.format(domain=DOMAIN_, id = appid, key = self.key)
+		try:
+			response = urllib.request.urlopen(url).read().decode('gbk', 'ignore')
+			data = json.loads(response)
+			print(data)
+		except urllib.error.HTTPError as e:
+			content = e.read()
+
 	def getAPIDictWithKey(self):
 		pass
 
@@ -35,9 +48,12 @@ class SteamAPI:
 		pass
 
 
+
+
 def main():
     steamapi = SteamAPI()
     dic = steamapi.getAppsDict()
+    steamapi.getAppsPriceDict(730)
 
 
 if __name__ == "__main__":
